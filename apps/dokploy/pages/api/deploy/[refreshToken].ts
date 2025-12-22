49442fcf6a18c1994309e634148d317df9019f22
weareaisle-qa-app <Iv23liFuoUR2xEzvwa00+weareaisle-qa-app@users.noreply.github.com>
2025-12-22 19:57:43 +0000

    Apply fix for issue: Improper Type Validation in refreshtoken.ts at line 302

--- apps/dokploy/pages/api/deploy/[refreshToken].ts
+++ apps/dokploy/pages/api/deploy/[refreshToken].ts
@@ -260,9 +260,7 @@ export const extractCommitMessage = (headers: any, body: any) => {
 
 	// Bitbucket
 	if (headers["x-event-key"]?.includes("repo:push")) {
-		return body.push.changes && body.push.changes.length > 0
-			? body.push.changes[0].new.target.message
-			: "NEW COMMIT";
+		return body?.push?.changes?.[0]?.new?.target?.message || "NEW COMMIT";
 	}
 
 	// Gitea
@@ -299,9 +297,7 @@ export const extractHash = (headers: any, body: any) => {
 
 	// Bitbucket
 	if (headers["x-event-key"]?.includes("repo:push")) {
-		return body.push.changes && body.push.changes.length > 0
-			? body.push.changes[0].new.target.hash
-			: "NEW COMMIT";
+		return body?.push?.changes?.[0]?.new?.target?.hash || "NEW COMMIT";
 	}
 
 	// Gitea
@@ -322,7 +318,7 @@ export const extractBranchName = (headers: any, body: any) => {
 	}
 
 	if (headers["x-event-key"]?.includes("repo:push")) {
-		return body?.push?.changes[0]?.new?.name;
+		return body?.push?.changes?.[0]?.new?.name;
 	}
 
 	return null;
