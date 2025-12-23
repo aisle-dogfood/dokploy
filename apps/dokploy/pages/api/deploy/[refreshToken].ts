7732fbf4a53d78f1cc53d86fca3ca96031be0782
weareaisle-qa-app <Iv23liFuoUR2xEzvwa00+weareaisle-qa-app@users.noreply.github.com>
2025-12-23 02:05:30 +0000

    Apply fix for issue: Improper Type Validation in refreshtoken.ts at line 263

--- apps/dokploy/pages/api/deploy/[refreshToken].ts
+++ apps/dokploy/pages/api/deploy/[refreshToken].ts
@@ -237,7 +237,7 @@ export const extractImageTagFromRequest = (
 	headers: any,
 	body: any,
 ): string | null => {
-	if (headers["user-agent"]?.includes("Go-http-client")) {
+	if (typeof headers["user-agent"] === "string" && headers["user-agent"].includes("Go-http-client")) {
 		if (body.push_data && body.repository) {
 			return body.push_data.tag;
 		}
@@ -259,7 +259,7 @@ export const extractCommitMessage = (headers: any, body: any) => {
 	}
 
 	// Bitbucket
-	if (headers["x-event-key"]?.includes("repo:push")) {
+	if (typeof headers["x-event-key"] === "string" && headers["x-event-key"].includes("repo:push")) {
 		return body.push.changes && body.push.changes.length > 0
 			? body.push.changes[0].new.target.message
 			: "NEW COMMIT";
@@ -272,7 +272,7 @@ export const extractCommitMessage = (headers: any, body: any) => {
 			: "NEW COMMIT";
 	}
 
-	if (headers["user-agent"]?.includes("Go-http-client")) {
+	if (typeof headers["user-agent"] === "string" && headers["user-agent"].includes("Go-http-client")) {
 		if (body.push_data && body.repository) {
 			return `Docker image pushed: ${body.repository.repo_name}:${body.push_data.tag} by ${body.push_data.pusher}`;
 		}
@@ -298,7 +298,7 @@ export const extractHash = (headers: any, body: any) => {
 	}
 
 	// Bitbucket
-	if (headers["x-event-key"]?.includes("repo:push")) {
+	if (typeof headers["x-event-key"] === "string" && headers["x-event-key"].includes("repo:push")) {
 		return body.push.changes && body.push.changes.length > 0
 			? body.push.changes[0].new.target.hash
 			: "NEW COMMIT";
@@ -321,7 +321,7 @@ export const extractBranchName = (headers: any, body: any) => {
 		return body?.ref ? body?.ref.replace("refs/heads/", "") : null;
 	}
 
-	if (headers["x-event-key"]?.includes("repo:push")) {
+	if (typeof headers["x-event-key"] === "string" && headers["x-event-key"].includes("repo:push")) {
 		return body?.push?.changes[0]?.new?.name;
 	}
 
@@ -341,7 +341,7 @@ export const getProviderByHeader = (headers: any) => {
 		return "gitlab";
 	}
 
-	if (headers["x-event-key"]?.includes("repo:push")) {
+	if (typeof headers["x-event-key"] === "string" && headers["x-event-key"].includes("repo:push")) {
 		return "bitbucket";
 	}
 
